76a07dbce6f60afb3ee37588807e5a13705da582
robings <r_gordon_stables@hotmail.com>
2020-04-09 12:02:20 +0100

    added create route for accounts (works), response handling does not work

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ const url = 'mongodb://localhost:27017';
 
 app.use(bodyParser.json());
 
+//landing route
 app.get('/', (req, res) => {
-    res.send('<h1>Hippos Banking App</h1>');
+    res.send('<h1>Hippos Banking App</h1>use url /accounts to use the API');
 })
 
+//route to get all accounts
 app.get('/accounts', (req, res) => {
     MongoClient.connect(url, {useUnifiedTopology: true}, async (err, client) => {
         let db = client.db(dbName);
@@ -42,6 +44,40 @@ let getUsers = async(db) => {
     let result = await collection.find({deleted:false}).toArray();
     return result
 };
+
+//route to add an account
+app.post('/accounts', (req, res) => {
+    const dataToSend = {
+        name: req.body.name,
+        balance: req.body.balance
+    };
+
+    MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, async (err, client) => {
+        console.log ('Connect to MongoDb');
+
+        let db = client.db(dbName)
+
+        //run a function that adds data to db
+        let docs = await insertDataInDb(db, dataToSend);
+            if (docs.insertedCount === 1) {
+                res.send('It Worked')
+            } else {
+                res.send('It did nay work')
+            }
+            client.close()
+    })
+})
+
+let insertDataInDb = async (db, dataToSend) => {
+        let collection = db.collection(collectionAccounts);
+        collection.insertOne(dataToSend, (err, docs) => {
+           console.log (docs);
+        })
+        return await didItWork;
+}
+
+
+
 
 app.listen(PORT, ()=> {
     console.log(`Hippos Banking App listening on port ${PORT}`);
