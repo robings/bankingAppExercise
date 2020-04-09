9327a4788c6fc6d1112edb3e833892bbcce36c38
robings <r_gordon_stables@hotmail.com>
2020-04-09 10:57:19 +0100

    created get route for all accounts

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 const bodyParser =  require('body-parser');
 const app = express();
 const PORT = 4000;
+const dbName = 'hipposBank';
+const collectionAccounts = 'accounts';
 
 const url = 'mongodb://localhost:27017';
 
@@ -13,8 +16,20 @@ app.get('/', (req, res) => {
 })
 
 app.get('/accounts', (req, res) => {
+    MongoClient.connect(url, {useUnifiedTopology: true}, async (err, client) => {
+        let db = client.db(dbName);
+        let documentsReturned = await getUsers(db);
+        await res.send(documentsReturned);
+    });
+});
 
-})
+let getUsers = async(db) => {
+    let collection = db.collection(collectionAccounts);
+    let result = await collection.find({deleted:false}).toArray();
+    return result
+};
+
+
 
 app.listen(PORT, ()=> {
     console.log(`Hippos Banking App listening on port ${PORT}`);
